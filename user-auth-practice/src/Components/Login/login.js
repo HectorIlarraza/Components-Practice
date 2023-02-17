c8d825265a7c7388a5bd96584caed749db06f611
HectorIlarraza <53978521+HectorIlarraza@users.noreply.github.com>
2023-02-16 21:54:02 -0600

    changed structure of login JS file and added CSS to it

--- user-auth-practice/src/Components/Login/login.js
+++ user-auth-practice/src/Components/Login/login.js
@@ -1,61 +1,86 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import "./login.css";
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-  
-    const handleEmailChange = (e) => {
-      setEmail(e.target.value);
-    };
-  
-    const handlePasswordChange = (e) => {
-      setPassword(e.target.value);
-    };
-  
-    const handleLoginFormSubmit = (e) => {
-      e.preventDefault();
-  
-      // Here we would make an API request to authenticate the user
-      // and receive a token if authentication is successful.
-      // For simplicity, we'll just hardcode the token here.
-      const authToken = 'my-auth-token';
-  
-      // If we have a token, set the `isLoggedIn` state to `true`.
-      if (authToken) {
-        setIsLoggedIn(true);
-      }
-    };
-  
-    const handleLogoutButtonClick = () => {
-      // Clear the token from local storage or cookies, depending on the implementation.
-      // For simplicity, we'll just remove it from state here.
-      setIsLoggedIn(false);
-    };
-  
-  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLoginFormSubmit = (e) => {
+    e.preventDefault();
+
+    // Here we would make an API request to authenticate the user
+    // and receive a token if authentication is successful.
+    // For simplicity, we'll just hardcode the token here.
+    const authToken = "my-auth-token";
+
+    // If we have a token, set the `isLoggedIn` state to `true`.
+    if (authToken) {
+      setIsLoggedIn(true);
+    }
+  };
+
+  const handleLogoutButtonClick = () => {
+    // Clear the token from local storage or cookies, depending on the implementation.
+    // For simplicity, we'll just remove it from state here.
+    setIsLoggedIn(false);
+  };
+
+  const handleSwitchMode = () => {
+    setIsLoggedIn((prevState) => !prevState);
+  };
+
   return (
-    <div className='auth-component'>
-      {isLoggedIn ? (
-        <div>
-          <h2>You are logged in!</h2>
-          <button onClick={handleLogoutButtonClick}>Log out</button>
-        </div>
-      ) : (
-        <form onSubmit={handleLoginFormSubmit}>
-          <label>
-            Email:
-            <input className="input-field" type="email" value={email} onChange={handleEmailChange} />
-          </label>
-          <label>
-            Password:
-            <input className="input-field" type="password" value={password} onChange={handlePasswordChange} />
-          </label>
-          <button className="login-btn" type="submit">Log in</button>
-        </form>
-      )}
+    <div className="auth-container">
+      <div className="auth-form">
+      <h2>{isLoggedIn ? "You are logged in!" : "Log In"}</h2>
+        {isLoggedIn ? (
+          <div>
+            <button onClick={handleLogoutButtonClick}>Log out</button>
+          </div>
+        ) : (
+          <form onSubmit={handleLoginFormSubmit}>
+            <div className="auth-group">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                id="email"
+                value={email}
+                onChange={handleEmailChange}
+              />
+            </div>
+            <div className="auth-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </div>
+            <p>
+              <div className="auth-group">
+                <label htmlFor="login-state">
+                  {isLoggedIn ? "Don't have an account?" : "Already have an account?"}
+                </label>
+              </div>{" "}
+              <button type="button" onClick={handleSwitchMode}>
+                {isLoggedIn ? "Sign up" : "Login"}
+              </button>
+            </p>
+          </form>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
